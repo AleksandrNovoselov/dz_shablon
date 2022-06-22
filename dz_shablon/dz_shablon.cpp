dc2237e3de5dace4cccd71e6cd8deb0858d31b6b
User <User@NOUT>
2022-06-22 20:04:22 +0400

    Добавьте файлы проекта.

---
+++ dz_shablon/dz_shablon.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime> 
+using namespace std;
+
+template <class T> vector<T> random(vector<T> &arr)
+{
+    srand(time(NULL));
+    for (auto i = arr.begin(); i < arr.end(); i++)
+    {
+        *i = 'a' + rand()%10;
+    }
+    return arr;
+}
+
+template <class T> T findMin(vector<T>& arr)
+{
+    auto Min = arr.begin();
+    for (auto i = arr.begin(); i < arr.end(); i++)
+    {
+        if (*i < *Min)
+        {
+            Min = i;
+        }
+    }
+    return *Min;
+}
+template <class T> T findMax(vector<T>& arr)
+{
+    auto Max = arr.begin();
+    for (auto i = arr.begin(); i < arr.end(); i++)
+    {
+        if (*i > *Max)
+        {
+            Max = i;
+        }
+    }
+    return *Max;
+}
+template <class T> void print(vector<T>& arr)
+{
+    for (auto i = arr.begin(); i < arr.end(); i++)
+    {
+        cout << *i<<"\t";
+    }
+    cout << endl;
+}
+int main()
+{
+    vector<int> vecI(3);
+    vector<float> vecF(3);
+    vector<string> vecS(10);
+
+    random<int>(vecI);
+    print(vecI);
+
+    random<string>(vecS);
+    print(vecS);
+}
+
